6d70eb645342d79c8031f4894d688a949aee9f36
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-02-25 12:56:38 +0100

    Corrections sur les fonctions du test de Miller

--- p1exo1.c
+++ p1exo1.c
@@ -69,6 +69,7 @@ void plus_grand_premier_is_prime_naive_en_temps_donne(long p, double s){
 	printf("Dernier nombre premier trouvé=%ld, temps CPU=%f\n", dernier_premier, dernier_premier_CPU);
 }
 
+/*Teste si a est un temoin de Miller pour p, pour un entier a donne*/
 int witness (long a, long b, long d, long p){
     long x = modpow(a, d, p);
     if (x == 1){
@@ -83,31 +84,37 @@ int witness (long a, long b, long d, long p){
     return 1; 
 }
 
+/*Retourne un entier long genere aleatoirement entre low et up inclus*/
 long rand_long(long low, long up){ 
-    return rand() % (up - low +1)+low;
+    return rand() % (up - low + 1) + low;
 }
 
+/*Realise le test de Miller-Rabin en generant k valeurs
+de a au hasard, et en testant si chaque valeur de a est un temoin de Miller pour p.
+La fonction retourne 0 des qu’un temoin de Miller est trouve (p n’est pas premier), 
+et retourne 1 si aucun temoin de Miller n’a ete trouve (p est probablement premier)*/
 int is_prime_miller(long p, int k) { 
     if (p == 2) {
         return 1; 
     }
-    if (!(p & 1) || p <= 1) { //on verifie que p est impair et different de 1 return 0;
+    if (!(p & 1) || p <= 1) { //on verifie que p est impair et different de 1
+        return 0;
     }
     //on determine b et d :
     long b = 0;
     long d = p - 1;
     while (!(d & 1)){ //tant que d n’est pas impair
-        d = d/2;
-        b=b+1; 
+        d = d / 2;
+        b = b + 1; 
     }
     // On genere k valeurs pour a, et on teste si c’est un temoin :
     long a;
     int i;
-    for(i = 0;i<k;i++){
+    for(i = 0; i < k; i++){
         a = rand_long(2, p-1);
-        if(witness(a,b,d,p)){ 
+        if (witness(a,b,d,p)){ 
             return 0;
-        } 
+        }
     }
     return 1; 
 }
