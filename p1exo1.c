02ad1f71552998e0af29425fcf1ae26197a02dba
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-02-25 10:25:54 +0100

    Ajoute le test pour calculer le plus grand premier calculable en moins de 2 milliemes de secondes

--- p1exo1.c
+++ p1exo1.c
@@ -49,20 +49,33 @@ long modpow(long a,long m,long n){
     }
 }
 
+
+
 int main(){
     int max =10000;
     int res=0;
     int res2=0;
-	printf("is_prime_naive=%d\n",is_prime_naive(99));
-    // for(int i=3;i<max;i++){
-    // temps_initial = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
-    // is_prime_naive(i);
-    // temps_final = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
+	long p = 100001;
+	long dernier_premier;
 
-    // //On convertit les ‘‘ticks’’ consomm’es par fct en secondes :
-    // temps_cpu1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
-    // //printf("%d,%f\n",i,temps_cpu1);
-    // }
+	//Calcul du plus grand nombre premier trouvable en moins de 2 millemes de secondes avec is_prime_naive
+	temps_cpu1 = 0;
+    while (temps_cpu1 < 0.002){
+		temps_initial = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
+		res = is_prime_naive(p);
+		temps_final = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
+		//On convertit les ‘‘ticks’’ consomm’es par fct en secondes :
+		temps_cpu1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+        printf("p=%ld, premier=%d, temps CPU=%f\n", p, res, temps_cpu1);
+		if (res == 1){
+			dernier_premier = p; //sauvegarde le dernier nombre premier trouve
+            res = 0;
+        }
+		p+=2;
+    }
+	printf("Dernier nombre premier trouvé=%ld, temps CPU=%f\n", dernier_premier, temps_cpu1);
+
+
     // res=modpow_naive(142,7,187);
     // printf("%d\n",res);
     // res2=modpow(142,7,187);
