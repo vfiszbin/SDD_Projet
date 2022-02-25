c58e135dd630c673950b71bf121335f928d5a4f6
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-02-25 09:49:48 +0100

    Modifications sur is_prime_naive

--- p1exo1.c
+++ p1exo1.c
@@ -6,22 +6,21 @@
 clock_t temps_initial;
 clock_t temps_final;
 double temps_cpu1 , temps_cpu2;
+
+/*Determine si un entier p est premier ou non*/
 int is_prime_naive(long p){
     int i;
-    int cpt=0;
-    if (p<3){
-        exit(1);}
-    else{
-        for(i=3;i<p;i++){
-            if(p%i==0){
-                cpt++;
-            }
-        }
-        if (cpt==2 && p%2==1){
-            return 1;
-        }
-        return 0;
-    }
+	if (p == 2) //cas particulier, 2 est le seul nombre premier pair
+		return 1;
+	if (p < 3 || p % 2 == 0) //si le nombre est inférieur a 3 ou n'est pas impair, il ne peut pas etre premier
+		return 0;
+
+	for(i=3; i<p; i++){
+		if(p%i==0) //si l'un de ces entiers est diviseur de p, alors p n'est pas premier
+			return 0;
+	}
+	return 1;
+
 }
 long modpow_naive(long a,long m, long n){
     int i=0,res=1;
@@ -54,18 +53,19 @@ int main(){
     int max =10000;
     int res=0;
     int res2=0;
-    for(int i=3;i<max;i++){
-    temps_initial = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
-    is_prime_naive(i);
-    temps_final = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
+	printf("is_prime_naive=%d\n",is_prime_naive(99));
+    // for(int i=3;i<max;i++){
+    // temps_initial = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
+    // is_prime_naive(i);
+    // temps_final = clock () ; //Nombre de ‘‘ticks’’ consomm’es par le programme jusqu’ici
 
-    //On convertit les ‘‘ticks’’ consomm’es par fct en secondes :
-    temps_cpu1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
-    //printf("%d,%f\n",i,temps_cpu1);
-    }
-    res=modpow_naive(142,7,187);
-    printf("%d\n",res);
-    res2=modpow(142,7,187);
-    printf("%d\n",res2);
+    // //On convertit les ‘‘ticks’’ consomm’es par fct en secondes :
+    // temps_cpu1 = ((double)(temps_final - temps_initial))/CLOCKS_PER_SEC;
+    // //printf("%d,%f\n",i,temps_cpu1);
+    // }
+    // res=modpow_naive(142,7,187);
+    // printf("%d\n",res);
+    // res2=modpow(142,7,187);
+    // printf("%d\n",res2);
 
 }
