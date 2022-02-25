d2a0aab09eb9bb7cb645e5468b42ca2b9ff48e61
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-02-25 11:55:10 +0100

    Ajoute les fonctions du test de Miller

--- p1exo1.c
+++ p1exo1.c
@@ -69,6 +69,50 @@ void plus_grand_premier_is_prime_naive_en_temps_donne(long p, double s){
 	printf("Dernier nombre premier trouvé=%ld, temps CPU=%f\n", dernier_premier, dernier_premier_CPU);
 }
 
+int witness (long a, long b, long d, long p){
+    long x = modpow(a, d, p);
+    if (x == 1){
+        return 0;
+    }
+    for(long i = 0; i < b; i++){ 
+        if(x == p-1){
+            return 0; 
+        }
+        x = modpow(x,2,p); 
+    }
+    return 1; 
+}
+
+long rand_long(long low, long up){ 
+    return rand() % (up - low +1)+low;
+}
+
+int is_prime_miller(long p, int k) { 
+    if (p == 2) {
+        return 1; 
+    }
+    if (!(p & 1) || p <= 1) { //on verifie que p est impair et different de 1 return 0;
+    }
+    //on determine b et d :
+    long b = 0;
+    long d = p - 1;
+    while (!(d & 1)){ //tant que d n’est pas impair
+        d = d/2;
+        b=b+1; 
+    }
+    // On genere k valeurs pour a, et on teste si c’est un temoin :
+    long a;
+    int i;
+    for(i = 0;i<k;i++){
+        a = rand_long(2, p-1);
+        if(witness(a,b,d,p)){ 
+            return 0;
+        } 
+    }
+    return 1; 
+}
+
+
 int main(){
     long res;
     long res2;
