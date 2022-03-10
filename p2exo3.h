513e378f069c20f1ac71bb9bfd71392400d878de
davytse94 <79249404+davytse94@users.noreply.github.com>
2022-03-10 12:31:09 +0100

    p2exo3.h

---
+++ p2exo3.h
@@ -0,0 +1,25 @@
+#ifndef P2EXO3_H
+
+#define P2EXO3_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+typedef struct key{
+    long val;
+    long n;
+} Key;
+
+typedef struct signature{
+    long* tab;
+    int taille;
+} Signature;
+
+
+void init_key(Key* key, long val, long n );
+void init_pair_keys(Key* pkey,Key* skey, long low_size, long up_size);
+char* key_to_str (Key* key);
+Key* str_to_key(char* str);
+
+#endif
