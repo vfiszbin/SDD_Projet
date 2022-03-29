98bd4cccb87c81bba089ad14a139b01c63c0ae3e
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-03-29 16:26:32 +0200

    Modifications p3main

--- p3main.c
+++ p3main.c
@@ -5,7 +5,7 @@ int main(){
 	CellKey *lck = read_public_keys("candidates.txt");
 	if (!lck)
 		return 1;
-	printf("\nListe chaînée de déclarations clées lues avec read_public_keys :\n");
+	printf("\nListe chaînée de clés lues avec read_public_keys :\n");
 	print_list_keys(lck);
 
 	delete_list_keys(lck);
@@ -21,7 +21,6 @@ int main(){
 	print_list_cell_protected(lcp);
 
 	//Test de supprime_declarations_non_valides
-	
 	supprime_declarations_non_valides(&lcp);
 	printf("\nListe chaînée de déclarations signées après la suppression des déclarations dont la signature est invalide :\n");
 	print_list_cell_protected(lcp);
