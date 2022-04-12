48b1c27e2a812d69bacc9164c5179b852b9d4505
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-04-12 21:14:34 +0200

    Test et correction ecrire_block

--- p4exo7.c
+++ p4exo7.c
@@ -2,42 +2,44 @@
 
 #define BUFFLEN 1024
 
-void ecrire_fichier(char* nom, Block* b){
+int ecrire_block(char* nom, Block* b){
     FILE* f;
     f=fopen(nom,"w");
     if (!f){
 		printf("Erreur d'ouverture de %s\n", nom);
-		return NULL;
+		return 0;
 	}
     Protected * pr;
+    CellProtected *vote;
     char*pr_str;
     if (b){
-        //tu pense que on peut avoir plusieurs vote ?
-        fprintf("(%lx,%lx)\n",b->author->val, b->author->n);
-        fprintf("%d\n",b->nb_votes);
-        while(b->votes){
-            pr=init_protected(b->votes->data->pKey,b->votes->data->mess,b->votes->data->sgn);
+        fprintf(f, "(%lx,%lx)\n", b->author->val, b->author->n);
+        fprintf(f, "%d\n",b->nb_votes);
+        vote = b->votes;
+        while(vote){
+            pr=init_protected(b->votes->data->pKey, b->votes->data->mess, b->votes->data->sgn);
             if (!pr){
                 fclose(f);
-                return NULL;
+                return 0;
             }
             pr_str=protected_to_str(pr);
             if (!pr_str){
                 fclose(f);
                 free(pr);
-                return NULL;
+                return 0;
             }
-            fprintf("%s\n", pr_str);
-            b->votes->next;
+            fprintf(f, "%s\n", pr_str);
+            vote = vote->next;
         }
-        fprintf("%s\n",b->hash);
-        fprintf("%s\n",b->previous_hash);
-        fprintf("%d\n",b->nonce);
+        fprintf(f, "%s\n",b->hash);
+        fprintf(f, "%s\n",b->previous_hash);
+        fprintf(f, "%d\n",b->nonce);
     }
     fclose(f);
+    return 1;
 }
 
-Block * create_Block(Key *author, CellProtected *votes, unsigned char *hash, unsigned char *previous_hash, int nonce){
+Block * create_Block(Key *author, CellProtected *votes, unsigned char *hash, unsigned char *previous_hash, int nonce, int nb_votes){
     Block* b =(Block*)malloc(sizeof(Block));
     if(!b){
         return NULL;
@@ -47,10 +49,11 @@ Block * create_Block(Key *author, CellProtected *votes, unsigned char *hash, uns
     b->hash=hash;
     b->previous_hash=previous_hash;
     b->nonce=nonce;
+    b->nb_votes = nb_votes;
     return b;
 }
 
-Block* lire_fichier(char* nom){
+Block* lire_block(char* nom){
     FILE* f = fopen(nom,"r");
     if (f==NULL){
 		printf("Erreur d'ouverture de %s\n", nom);
@@ -128,7 +131,7 @@ Block* lire_fichier(char* nom){
         fclose(f);
         return NULL;
     }
-    if (sscanf(buffer,"%s",&hash) != 1){
+    if (sscanf(buffer,"%s",hash) != 1){
         delete_list_cell(lcp);
         free(cle);
         fclose(f);
@@ -142,7 +145,7 @@ Block* lire_fichier(char* nom){
         fclose(f);
         return NULL;
     }
-    if (sscanf(buffer,"%s", &hash_precedent) != 1){
+    if (sscanf(buffer,"%s", hash_precedent) != 1){
         delete_list_cell(lcp);
         free(cle);
         fclose(f);
@@ -164,7 +167,7 @@ Block* lire_fichier(char* nom){
     }
 
 
-    Block *b = create_Block(cle,lcp,hash,hash_precedent,nonce);
+    Block *b = create_Block(cle,lcp,hash,hash_precedent,nonce, nb_votes);
     if(!b){       
         delete_list_cell(lcp);
         free(cle);
@@ -173,79 +176,110 @@ Block* lire_fichier(char* nom){
     return b;
 }
 
-char* block_to_str(Block* block){
-    char* key = key_to_str(block->author);
-    if (!key)
-        return NULL;
-    char* listevotes = protected_to_str(block->votes->data);
-    if (!listevotes)
-        free(key);
-        return NULL;
-    int cpt=0;
-    while (block->votes){
-        cpt++;
-        block=block->votes->next;
-    }
-    int size = strlen(key)+strlen(listevotes)*cpt+strlen(block->previous_hash)+strlen(block->nonce)+3+cpt;
-    char* caractere = (char*)malloc(size*sizeof(char));
-    if(!caractere){
-        free(key);
-        free(listevotes);
-        return NULL;
-    }
-    int i=0;
-    int j=0;
-    while(key[j]){
-        caractere[i]=key[j];
-        i++;
-        j++;
-    }
-    caractere[i] = ' ';
-    i++;
-    j=0;
-    while(listevotes[j]){
-        caractere[i]=listevotes[j];
-        i++;
-        j++;
-    }
-    caractere[i] = ' ';
-    i++;
-    j=0;
-    while(block->previous_hash[j]){//pas sur de pouvoir copie ca 
-        caractere[i]=block->previous_hash[j];
-        i++;
-        j++;
-    }
-    caractere[i] = ' ';
-    i++;
-    caractere[i]=block->nonce;
-    i++;
-    caractere[i]='\0';
-    free(key);
-    free(listevotes);
-    return caractere;
-}
+// char* block_to_str(Block* block){
+//     char* key = key_to_str(block->author);
+//     if (!key)
+//         return NULL;
+//     char* listevotes = protected_to_str(block->votes->data);
+//     if (!listevotes)
+//         free(key);
+//         return NULL;
+//     int cpt=0;
+//     while (block->votes){
+//         cpt++;
+//         block=block->votes->next;
+//     }
+//     int size = strlen(key)+strlen(listevotes)*cpt+strlen(block->previous_hash)+strlen(block->nonce)+3+cpt;
+//     char* caractere = (char*)malloc(size*sizeof(char));
+//     if(!caractere){
+//         free(key);
+//         free(listevotes);
+//         return NULL;
+//     }
+//     int i=0;
+//     int j=0;
+//     while(key[j]){
+//         caractere[i]=key[j];
+//         i++;
+//         j++;
+//     }
+//     caractere[i] = ' ';
+//     i++;
+//     j=0;
+//     while(listevotes[j]){
+//         caractere[i]=listevotes[j];
+//         i++;
+//         j++;
+//     }
+//     caractere[i] = ' ';
+//     i++;
+//     j=0;
+//     while(block->previous_hash[j]){//pas sur de pouvoir copie ca 
+//         caractere[i]=block->previous_hash[j];
+//         i++;
+//         j++;
+//     }
+//     caractere[i] = ' ';
+//     i++;
+//     caractere[i]=block->nonce;
+//     i++;
+//     caractere[i]='\0';
+//     free(key);
+//     free(listevotes);
+//     return caractere;
+// }
+
 //question 7.5
-unsigned char* crypteensha256 (char* message){
-    unsigned char *d = SHA256(message,strlen(message),0);
-    int i;
-    for(i=0;i<SHA256_DIGEST_LENGTH;i++){
-        printf("%02x",d[i]);
-    }
-    putchar('\n');
-    return d;
-}
+// unsigned char* crypteensha256 (char* message){
+//     unsigned char *d = SHA256(message,strlen(message),0);
+//     int i;
+//     for(i=0;i<SHA256_DIGEST_LENGTH;i++){
+//         printf("%02x",d[i]);
+//     }
+//     putchar('\n');
+//     return d;
+// }
 
 //question 7.9
-void delete_block(Block *b){
-    CellProtected* tmp;
-    if(b!=NULL){
-        while(b->votes!=NULL){
-            tmp=b->votes;
-            b->votes=b->votes->next;
-            free(b->votes);
-        }
-        free(b->hash);
-        free(b->previous_hash);
-    }
+// void delete_block(Block *b){
+//     CellProtected* tmp;
+//     if(b!=NULL){
+//         while(b->votes!=NULL){
+//             tmp=b->votes;
+//             b->votes=b->votes->next;
+//             free(b->votes);
+//         }
+//         free(b->hash);
+//         free(b->previous_hash);
+//     }
+// }
+
+int main(){
+    Key *k = malloc(sizeof(Key));
+    k->n = 123;
+    k->val = 456;
+
+
+    CellProtected *lcp = read_protected("declarations.txt");
+	if (!lcp){
+		free(k);
+		return 1;
+	}
+    unsigned char h [] = "hash";
+    unsigned char ph [] = "prev_hash";
+
+
+    CellProtected* tmp = lcp;
+	int nb_votes = 0;
+	while(tmp){ //calcule la taile de la liste
+		nb_votes++;
+		tmp = tmp->next;
+	}
+
+    Block *b = create_Block(k, lcp, h, ph, 111, nb_votes);
+
+    ecrire_block("blocks.txt", b);
+
+    // Block *b2 = lire_block("blocks.txt");
 }
+
--- p4exo7.h
+++ p4exo7.h
@@ -13,9 +13,9 @@ typedef struct block{
 }Block;
 
 
-void ecrire_fichier(char* nom, Block* b);
-Block * create_Block(Key *author, CellProtected *votes, unsigned char *hash, unsigned char *previous_hash, int nonce);
-Block* lire_fichier(char* nom);
+int ecrire_block(char* nom, Block* b);
+Block * create_Block(Key *author, CellProtected *votes, unsigned char *hash, unsigned char *previous_hash, int nonce, int nb_votes);
+Block* lire_block(char* nom);
 char* block_to_str(Block* block);
 unsigned char* crypteensha256 (char* message);
 void delete_block(Block *b);
