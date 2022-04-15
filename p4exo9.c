140a565dec6247fe0a86a31231b0314813b2e349
davy tse <tsechunwai@hotmail.fr>
2022-04-15 22:43:03 +0200

    q9.2

--- p4exo9.c
+++ p4exo9.c
@@ -16,3 +16,27 @@ void submit_vote(Protected* pr){
     fclose(f);
 }
 
+void create_block(CellTree* tree,Key* author,int d){
+    CellProtected* votes = read_protected("Pending.txt");
+    CellTree* dernier=last_node(tree);
+
+    Block* b=(Block*)malloc(sizeof(Block));
+    if(!b){
+        delete_list_cell(vote);
+        return;
+    }
+    b->author;
+    b->votes=votes;
+    if(dernier!=NULL){
+        b->previous_hash=dernier->block->hash;
+    }else{
+        b->previous_hash=NULL;
+    }
+    b->nonce=0;
+    compute_proof_of_work(b,d);
+    dernier->firstChild=create_node(b);
+    update_height(dernier,dernier->firstChild);
+    ecrire_block("Pending_block",b);
+    remove("pending.txt");
+}
+
